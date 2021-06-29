9658dee90e1facc5ae9219cce68caff93a72ec08
duxiaoqing <duxiaoqing@baidu.com>
2021-06-29 18:32:14 +0800

    init

---
+++ src/tree-node/create-bst.js
@@ -0,0 +1,9 @@
+const TreeNode = require('./tree-node')
+
+/**
+ * 
+ * @param {array<number|string>} source  [1, null, 2, 2]
+ */
+const createBST = source => {
+
+}
---
+++ src/tree-node/tree-node.js
@@ -0,0 +1,9 @@
+class TreeNode {
+    constructor(val, left, right){
+        this.val = val
+        this.left = left === undefined ? null : left
+        this.right = right === undefined ? null : right
+    }
+}
+
+module.exports = TreeNode
