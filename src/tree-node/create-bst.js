69d0b347b3eeeebedc2cb2d0cf37a2543fbf6ce7
duxiaoqing <duxiaoqing@baidu.com>
2021-06-29 20:34:00 +0800

    feat: create bst

--- src/tree-node/create-bst.js
+++ src/tree-node/create-bst.js
@@ -5,5 +5,14 @@ const TreeNode = require('./tree-node')
  * @param {array<number|string>} source  [1, null, 2, 2]
  */
 const createBST = source => {
+    let parentNode = null
 
+    for (let i = 0; i < source.length; i+=3){
+        if (source[i] === undefined) break
+        const val = source[i]
+        const node = new TreeNode(val)
+
+        // if (){}
+
+    }
 }
