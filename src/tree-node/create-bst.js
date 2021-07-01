572da306c56b0f88328be5c2e58653152b99d5ef
duxiaoqing <duxiaoqing@baidu.com>
2021-07-01 18:28:52 +0800

    feat: add show tree

--- src/tree-node/create-bst.js
+++ src/tree-node/create-bst.js
@@ -1,36 +1,54 @@
 const TreeNode = require('./tree-node')
 
+const isEmpty = v => v === null || v === undefined
+
 /**
  * 
- * @param {array<number|string>} source  [1, null, 2, 2]
- * 
- * [1, 2, 3, 4, 5, 6, 7] => 
- * [1, null, 2, 2, 3, 5, 7]
+ * @param {array<number|string>} source  [1, null, 2, null, null, 2]
  */
 const createBST = source => {
-    if (source.length === 0 || source[i] === null) return new TreeNode(null)
-    // todo: 先去考虑有哪些树，再去考虑生成怎样的数组？？？
-    // 根节点距离下一个左子数下标距离
-    // （1）父节点，左右子数都在 -> next：根下标为左节点，step为2; 
-    // （2）父节点，左子树为null，右节点存在 -> next：根下标为右节点，step为1
-    // （3）父节点，左子数存在，右节点为null -> next：根下标为左节点，step为2
-    // （4）父节点，左右节点都为null -> 不处理
-    const parentQueue = [{
-        index: 0,
-        step: 1
-    }]
-    let rootNode = null
+    if (!Array.isArray(source) || source.length === 0) return null
+    // 从当前节点查找左子节点2k+1, 右子节点2k+2
     
-    while(parentNodeIndexStack.length > 0){
-        const parent = parentQueue.shift()
-        
+    let rootNode = null, node = null
 
+    for (let i = 0; i < source.length; i++){
+        const val = source[i]
+        if (i === 0){
+            node = new TreeNode(val)
+            rootNode = node
+        }
+        if (val === null) continue
+        
+        const leftVal = source[2 * i + 1]
+        const rightVal = source[2 * i + 2]
+
+        // console.log(i, leftVal, rightVal, isEmpty(leftVal))
+
+        if (!isEmpty(leftVal)) {
+            node.left = new TreeNode(leftVal)
+        }
+
+        if (!isEmpty(rightVal)) {
+            node.right = new TreeNode(rightVal)
+        }
+
+        if (!isEmpty(leftVal)){
+            node = node.left
+        } else if (!isEmpty(rightVal)){
+            node =node.right
+        }
     }
 
     return rootNode
 }
+
+
 // todo
 const createArrayFromBSTree = () => {}
+//todo 能不能把null缩减些
+const root = createBST([1, null, 15,  null, null, 23, null, null, null, null, null, 30, 35])
+TreeNode.showTree(root)
 
 
 module.exports = createBST
--- src/tree-node/tree-node.js
+++ src/tree-node/tree-node.js
@@ -4,6 +4,26 @@ class TreeNode {
         this.left = left === undefined ? null : left
         this.right = right === undefined ? null : right
     }
+
+    static showTree(node){
+        if (node === null) return
+
+        console.log(`======${node.val}=======`)
+        if (node.left) {
+            console.log(`${node.val}左节点：${node.left.val}`)
+        } else {
+            console.log(`${node.val}左节点：null`)
+        }
+        
+        if (node.right) {
+            console.log(`${node.val}右节点：${node.right.val}`)
+        } else {
+            console.log(`${node.val}右节点：null`)
+        }
+        
+        TreeNode.showTree(node.left)
+        TreeNode.showTree(node.right)
+    }
 }
 
 module.exports = TreeNode
